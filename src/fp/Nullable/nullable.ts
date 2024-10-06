cfb96ff8e91bf1e316f0f875bf589ff37acf51d9
dylanmsql <masquelierdylan1999@gmail.com>
2024-10-07 00:55:58 +0200

    add utils method

--- src/fp/Nullable/nullable.ts
+++ src/fp/Nullable/nullable.ts
@@ -1,1 +1,5 @@
 export type Nullable<A> = A | null | undefined;
+
+export const isNull = <A>(n: Nullable<A>): boolean => n === null;
+export const isUndefined = <A>(n: Nullable<A>): boolean => n === undefined;
+export const isNullOrUndefined = <A>(n: Nullable<A>): boolean => isNull(n) || isUndefined(n);
