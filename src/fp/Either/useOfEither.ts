caaf50178b6a8c2243dbea27f62283003de22534
dylanmsql <77782703+dylanmsql@users.noreply.github.com>
2024-11-03 23:12:54 +0100

    refacto to made the code more readable

--- src/fp/Either/useOfEither.ts
+++ src/fp/Either/useOfEither.ts
@@ -1,56 +1,58 @@
-import * as E from "./either"
-import { makeFoldHandlersWithField } from "../Utils/utils"
-import { pipe } from "../function"
+import * as E from "./either";
+import { makeFoldHandlersWithField } from "../Utils/utils";
+import { pipe } from "../function";
 
 type NotEnoughBalance = Readonly<{
     type: 'NotEnoughBalance',
     message: string
-}>
+}>;
 
 type AccountFrozen = Readonly<{
     type: 'AccountFrozen',
     message: string
-}>
+}>;
 
 type Item = Readonly<{
     id: number,
     name: string,
     cost: number
-}>
+}>;
 
 type BankAccount = Readonly<{
     balance: number,
     isFrozen: boolean,
     currency: string
-}>
+}>;
 
 type Cart = Readonly<{
     items: Item[],
     total: number
 }>;
 
-const pay = 
-    (amount: number) =>
-    (account: BankAccount): E.Either<AccountFrozen | NotEnoughBalance, BankAccount> => 
-        account.isFrozen
-            ? E.left({
+type PaymentError = AccountFrozen | NotEnoughBalance;
+
+const pay = (amount: number) => 
+    (account: BankAccount): E.Either<PaymentError, BankAccount> => {
+        if (account.isFrozen) {
+            return E.left({
                 type: 'AccountFrozen',
                 message: 'Cannot pay with a frozen account'
-            })
-            : account.balance < amount
-                ? E.left({
-                    type: 'NotEnoughBalance',
-                    message: `Cannot pay ${amount}${account.currency} with a balance of ${account.balance}${account.currency}`
-                })
-                : E.right({
-                    ...account,
-                    balance: account.balance - amount
-                })
+            });
+        }
+        if (account.balance < amount) {
+            return E.left({
+                type: 'NotEnoughBalance',
+                message: `Cannot pay ${amount}${account.currency} with a balance of ${account.balance}${account.currency}`
+            });
+        }
+        return E.right({
+            ...account,
+            balance: account.balance - amount
+        });
+    };
 
 const foldError = makeFoldHandlersWithField('type');
-const checkout = 
-    (cart: Cart) =>
-    (account: BankAccount) =>
+const checkout = (cart: Cart) => (account: BankAccount): string =>
     pipe(
         account,
         pay(cart.total),
@@ -59,33 +61,24 @@ const checkout =
                 AccountFrozen: (e) => e.message,
                 NotEnoughBalance: (e) => e.message
             }),
-            (a) => `The new balnce is ${a.balance}${a.currency}`
+            (updatedAccount) => `The new balance is ${updatedAccount.balance}${updatedAccount.currency}`
         )
     );
 
-console.log('No Problem : ', checkout({
-    total: 10,
-    items : []
-})({
-    balance: 20,
-    isFrozen: false,
-    currency: '€'
-}));
+const applyCheckout = (cart: Cart, account: BankAccount): string => 
+    checkout(cart)(account);
 
-console.log('Frozen problem : ', checkout({
-    total: 10,
-    items : []
-})({
-    balance: 20,
-    isFrozen: true,
-    currency: '€'
-}));
+console.log('No Problem:', applyCheckout(
+    { total: 10, items: [] },
+    { balance: 20, isFrozen: false, currency: '€' }
+));
 
-console.log('balance problem : ', checkout({
-    total: 30,
-    items : []
-})({
-    balance: 20,
-    isFrozen: false,
-    currency: '€'
-}));
+console.log('Frozen problem:', applyCheckout(
+    { total: 10, items: [] },
+    { balance: 20, isFrozen: true, currency: '€' }
+));
+
+console.log('Balance problem:', applyCheckout(
+    { total: 30, items: [] },
+    { balance: 20, isFrozen: false, currency: '€' }
+));
